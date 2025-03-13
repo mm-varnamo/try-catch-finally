f204c989a2b23bfa55c20bd1144cf170ad28bdae
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-03-13 17:58:36 +0100

    feat: add temporary imprint and privacy page

---
+++ frontend/src/pages/imprint.tsx
@@ -0,0 +1,10 @@
+const ImprintPage = () => {
+	return (
+		<div>
+			<h1>Imprint</h1>
+			<p>Contact info goes here.</p>
+		</div>
+	);
+};
+
+export default ImprintPage;
---
+++ frontend/src/pages/privacy.tsx
@@ -0,0 +1,10 @@
+const PrivacyPage = () => {
+	return (
+		<div>
+			<h1>Privacy</h1>
+			<p>We care about your privacy.</p>
+		</div>
+	);
+};
+
+export default PrivacyPage;
