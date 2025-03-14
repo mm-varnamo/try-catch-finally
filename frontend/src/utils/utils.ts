abb2ec9ee07c108273263b2bb7386946c93eebf9
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-03-14 16:43:22 +0100

    feat: add util function for generating slugs

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,5 +1,14 @@
 import format from 'date-fns/format';
 
+export const generateSlug = (input: string) => {
+	return input
+		.replace(/[^a-zA-Z0-9 ]/g, '')
+		.trim()
+		.replace(/ +/g, ' ') // merge multiple spaces into one space
+		.replace(/\s/g, '-') // replace spaces with "-"
+		.toLowerCase();
+};
+
 export const formatDate = (dateString: string) => {
 	return format(new Date(dateString), 'MMM d, yyyy');
 };
