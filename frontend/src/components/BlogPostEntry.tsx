635b51fe3d810663e2ca57465475a94af5478508
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-03-17 23:15:10 +0100

    feat: implement responsive images for all posts

--- frontend/src/components/BlogPostEntry.tsx
+++ frontend/src/components/BlogPostEntry.tsx
@@ -2,6 +2,7 @@ import { BlogPost } from '@/models/blog-post';
 import { formatDate } from '@/utils/utils';
 import Link from 'next/link';
 import { Card } from 'react-bootstrap';
+import Image from 'next/image';
 
 interface BlogPostEntryProps {
 	post: BlogPost;
@@ -9,7 +10,7 @@ interface BlogPostEntryProps {
 }
 
 const BlogPostEntry = ({
-	post: { slug, title, summary, createdAt },
+	post: { slug, title, summary, featuredImageUrl, createdAt },
 	className,
 }: BlogPostEntryProps) => {
 	const postLink = '/blog/' + slug;
@@ -17,6 +18,15 @@ const BlogPostEntry = ({
 	return (
 		<Card className={className}>
 			<article>
+				<Link href={postLink}>
+					<Image
+						src={featuredImageUrl}
+						alt='Blog post featured image'
+						width={550}
+						height={200}
+						className='card-img-top object-fit-cover'
+					/>
+				</Link>
 				<Card.Body>
 					<Card.Title>
 						<Link href={postLink}>{title}</Link>
